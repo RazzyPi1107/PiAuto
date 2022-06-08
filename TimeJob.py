698174e013b5dca8ec1f3b9dc0fc51339ea54f16
RazzyPi1107 <107126642+RazzyPi1107@users.noreply.github.com>
2022-06-08 20:57:21 +0530

    Add files via upload

--- TimeJob.py
+++ TimeJob.py
@@ -4,14 +4,27 @@ import telepot
 import time
 import schedule
 import datetime
+from gpiozero import CPUTemperature
 
 tod = datetime.datetime.now()
 
 HHR = tod.hour
 MMN = tod.minute
+bot = telepot.Bot('1356204823:AAHY1lxuINcDabR6mfrRYMP-ojd11IcYna8')
+chat_id = '1047135684'
+
+
+bot.sendMessage(chat_id, str(msggg))
+
+
+cpu = CPUTemperature()
+print(cpu.temperature)
 
 #Monday is 0 and Sunday is 6
 dday = datetime.datetime.today().weekday()
 
 if HHR == 9 and MMN == 17:
     os.system('sudo python3 /home/kali/PiAuto/Loop.py')
+
+if HHR > 7 and HHR <= 23 and MMN == 00:
+    bot.sendMessage(chat_id, str(cpu.temperature))
